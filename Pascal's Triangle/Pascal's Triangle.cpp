217eaf9a6a25adb5d8d75bc0d9d265b6e6a7b6ff
Jing Zhang <jzhang_fl@outlook.com>
2024-10-29 10:31:14 -0500

    First Commit

---
+++ Pascal's Triangle/Pascal's Triangle.cpp
@@ -0,0 +1,49 @@
+// Pascal's Triangle.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+
+std::vector<int> PascalTriangleRow(int rowNo)
+{
+    std::vector<int> result(rowNo);
+    result[0] = 1;
+    if (rowNo == 1)
+        return result;
+
+    auto vi = PascalTriangleRow(rowNo - 1);
+    for (auto i = 1; i < rowNo - 1; ++i)
+    {
+        result[i] = vi[i - 1] + vi[i];
+    }
+
+    result[rowNo-1] = 1;
+
+    return result;
+}
+
+int main()
+{
+	while (true)
+	{
+		int count = 0;
+		std::cout << "Number of elements in the array: ";
+		std::cin >> count;
+		if (count == 0)
+			break;
+
+        auto vec = PascalTriangleRow(count);
+        bool isFirstElement = true;
+        for (auto i : vec)
+        {
+            if (!isFirstElement)
+                std::cout << "  ";
+            else
+                isFirstElement = false;
+
+            std::cout << i;
+        }
+
+		std::cout << std::endl;
+	}
+}
